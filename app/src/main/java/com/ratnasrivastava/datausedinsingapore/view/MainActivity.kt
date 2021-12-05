f1976f0227d92557124a157ced3d40e7e100e474
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2021-12-05 17:30:18 +0530

    initial commit

---
+++ app/src/main/java/com/ratnasrivastava/datausedinsingapore/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ratnasrivastava.datausedinsingapore.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ratnasrivastava.datausedinsingapore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
